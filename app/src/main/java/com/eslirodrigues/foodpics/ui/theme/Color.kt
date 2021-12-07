b5c1d6daabb3c1cfdf5775496b3f46e5ecc56603
Eslicdm <eslicdm1@gmail.com>
2021-12-07 12:20:54 -0300

    first commit

---
+++ app/src/main/java/com/eslirodrigues/foodpics/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.eslirodrigues.foodpics.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val DarkRed = Color(0xFF9b0000)
+val PrimaryRed = Color(0xFFd50000)
+val LightRed = Color(0xFFff5131)
